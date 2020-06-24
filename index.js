84753bafe0c97f4ea8e410154bb8b93cec479f5b
俞鹏飞 <yupengfei@aifin.ltd>
2020-06-24 18:25:11 +0800

    update code

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ var server = app.listen(4000, () => console.log('服务器正在4000端口号运
 var io = socket(server)
 
 io.on('connection', (socket) => {
-    console.log('实现socket连接')
-    console.log(socket.id)
+    // 获取从客户端发送的数据
+    socket.on('chat', (data) => {
+        io.sockets.emit('chat', data)
+    })
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data)
+    })
 })
--- public/chat.js
+++ public/chat.js
@@ -7,8 +7,27 @@ var message = document.getElementById('message'),
     feedback = document.getElementById('feedback')
 
 btn.addEventListener('click', () => {
+    // 客户端向服务端发送数据
     socket.emit('chat', {
         message: message.value,
         handle: handle.value,
     })
+})
+
+message.addEventListener('keypress', () => {
+    socket.emit('typing', handle.value)
+})
+
+// 获取从服务器传过来的数据
+socket.on('chat', (data) => {
+    feedback.innerHTML = ''
+    message.value = ''
+    output.innerHTML += `<p><strong>${data.handle}:${data.message}</strong></p>`
+})
+
+// 获取从服务端广播的数据
+
+socket.on('typing', (data) => {
+    console.log(data)
+    feedback.innerHTML = `<p><em>${data}正在输入......</em></p>`
 })
